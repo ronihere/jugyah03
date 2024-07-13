5a3c81c9f86a14c58c4fe3bd33074c7c52a858ee
ronihere <roni78rm@gmail.com>
2024-07-13 21:51:27 +0530

    build error resolved

--- public/svgs/index.tsx
+++ public/svgs/index.tsx
@@ -7,12 +7,12 @@ import BrandLogo from './brandlogo.svg';
 import FooterIcons from './footerIcons.svg';
 import ArrowIcon from './arrow.svg'
 import minimalicon2 from './minimalIcon2.svg'
-import minimalicon3 from './minimalIcon3.svg'
-import minimalicon4 from './minimalIcon4.svg'
-import minimalicon5 from './minimalIcon5.svg'
-import minimalicon6 from './minimalIcon6.svg'
-import minimalicon7 from './minimalIcon7.svg'
-import minimalicon8 from './minimalIcon8.svg'
+import minimalicon3 from './minimalicon3.svg'
+import minimalicon4 from './minimalicon4.svg'
+import minimalicon5 from './minimalicon5.svg'
+import minimalicon6 from './minimalicon6.svg'
+import minimalicon7 from './minimalicon7.svg'
+import minimalicon8 from './minimalicon8.svg'
 import SearchIcon from './searchIcon.svg'
 import HamIcon from './Hamburger.svg'
 export { Bed,HamIcon, ArrowIcon, minimalicon2, minimalicon3, minimalicon4, SearchIcon, minimalicon5, minimalicon6, minimalicon7, minimalicon8, BathTub, RoomArea, rightChevron, minimalCardIcon, BrandLogo, FooterIcons };
